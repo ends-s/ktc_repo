eedd1a4e8616dc0894936825f54ad11d36834646
ends-s <137268751+ends-s@users.noreply.github.com>
2023-07-11 14:16:18 +0300

    commit4

--- java/app.js
+++ java/app.js
@@ -112,5 +112,22 @@ let currentSet = {
     },
 }
 
-	console.log(currentSet.getSetupPrice());
+// Accordion
 
+const boxes = Array.from(document.querySelectorAll(".box"))
+
+boxes.forEach((box) => {
+    box.addEventListener("click", boxHandler)
+})
+
+function boxHandler(e) {
+    e.preventDefault();
+    let currentBox = e.target.closest(".box");
+    let currentContent = e.target.nextElementSibling;
+    currentBox.classList.toggle("active");
+    if (currentBox.classList.contains("active")) {
+        currentContent.style.maxHeight = currentContent.scrollHeight + "px";
+    } else {
+        currentContent.style.maxHeight = 0;
+    }
+}
